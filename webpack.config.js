3a9c57316290a470c07bc3c26af2e960399f8990
gangadharjannu <gangadhar014@gmail.com>
2020-04-07 22:59:50 +0200

    Adds MiniCssExtractPlugin plugin to extract css into a separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
         ],
       },
     ],
-  },
+  }
 };
