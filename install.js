70b9dbcaecfa6a02341b96f7250245f47e76f02a
gangadharjannu <gangadhar014@gmail.com>
2020-07-07 22:40:52 +0200

    Updates files with linting and prettifier

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
-var deferredInstallPrompt = null;
-var installButton = document.getElementById('butInstall');
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
 
 function saveBeforeInstallPromptEvent(evt) {
   deferredInstallPrompt = evt;
@@ -9,19 +9,21 @@ function saveBeforeInstallPromptEvent(evt) {
 function installPWA(evt) {
   deferredInstallPrompt.prompt();
   evt.srcElement.setAttribute('hidden', true);
-  deferredInstallPrompt.userChoice
-    .then(function (choice) {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
-    });
+  deferredInstallPrompt.userChoice.then(function onInstalledPrompt(choice) {
+    if (choice.outcome === 'accepted') {
+      // eslint-disable-next-line no-console
+      console.log(`User accepted the A2HS prompt ${choice}`);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`User dismissed the A2HS prompt ${choice}`);
+    }
+    deferredInstallPrompt = null;
+  });
 }
 
 function logAppInstalled(evt) {
-  console.log('Camic App was installed.', evt);
+  // eslint-disable-next-line no-console
+  console.log(`Camic App was installed. ${evt}`);
 }
 
 installButton.addEventListener('click', installPWA);
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = 'static-cache-v1';
-var FILES_TO_CACHE = [
+const CACHE_NAME = 'static-cache-v1';
+const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/favicon.png',
@@ -11,43 +11,55 @@ var FILES_TO_CACHE = [
   '/assets/apple-icon-167.png',
   '/assets/apple-icon-180.png',
   '/assets/manifest-icon-192.png',
-  '/assets/manifest-icon-512.png'
+  '/assets/manifest-icon-512.png',
 ];
 
-self.addEventListener('install', function (event) {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', function onInstall(event) {
+  // eslint-disable-next-line no-console
   console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then(function onCacheOpen(cache) {
+      // eslint-disable-next-line no-console
       console.log('[ServiceWorker] Pre-caching assets');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  // eslint-disable-next-line no-restricted-globals
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function (event) {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', function onActivate(event) {
+  // eslint-disable-next-line no-console
   console.log('[ServiceWorker] Activate');
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+    caches.keys().then(function onCache(keyList) {
+      return Promise.all(
+        keyList.map(function onKeyList(key) {
+          if (key !== CACHE_NAME) {
+            // eslint-disable-next-line no-console
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+          return null;
+        })
+      );
     })
   );
+  // eslint-disable-next-line no-restricted-globals
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', function onFetch(event) {
+  // eslint-disable-next-line no-console
   console.log('[ServiceWorker] Fetch', event.request.url);
   event.respondWith(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.match(event.request)
-        .then(function (response) {
-          return response || fetch(event.request);
-        });
+    caches.open(CACHE_NAME).then(function onCacheOpen(cache) {
+      return cache.match(event.request).then(function onCacheMatch(response) {
+        return response || fetch(event.request);
+      });
     })
   );
 });
