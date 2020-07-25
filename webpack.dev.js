c6bba311655a9ee49e12572f33a87ad895646686
gangadharjannu <gangadhar014@gmail.com>
2020-07-25 16:34:19 +0200

    TECHCNICAL - Adds separate webpack config for prod and dev build

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    https: true,
+    key: fs.readFileSync('./localhost-key.pem'),
+    cert: fs.readFileSync('./localhost.pem'),
+  },
+});
