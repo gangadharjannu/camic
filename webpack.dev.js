78842271a9e38a599c8a8eac3ec1545cd179b195
gangadharjannu <gangadhar014@gmail.com>
2020-07-27 14:39:54 +0200

    TECHNICAL - Configures webpack plugins

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    contentBase: './dist',
     hot: true,
     https: true,
     key: fs.readFileSync('./localhost-key.pem'),
