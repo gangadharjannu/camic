5e2220e600c9f14fed85b1e1bac6058ad6eccf8e
gangadharjannu <gangadhar014@gmail.com>
2020-07-27 14:39:54 +0200

    TECHNICAL - Configures webpack plugins

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,7 +11,39 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'favicon.png',
+          to: '.',
+        },
+        {
+          from: 'manifest.json',
+          to: '.',
+        },
+        {
+          from: 'install.js',
+          to: '.',
+        },
+        {
+          from: 'service-worker.js',
+          to: '.',
+        },
+        {
+          from: 'assets',
+          to: 'assets',
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      // Loads custom template
+      template: 'index.html',
+    }),
+  ],
   module: {
     rules: [
       {
